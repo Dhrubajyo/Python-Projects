32b1b48f51d2f93d6f60ada2ef025c256fde51af
DHRUBA <98461651+Dhrubajyo@users.noreply.github.com>
2022-03-15 13:52:21 +0530

    Add files via upload

---
+++ D Assistant/D Assistant.py
@@ -0,0 +1,14 @@
+import playsound
+print(".....................................................................Welcome to D Assistant....................................................................")
+name = input("Enter your name : ")
+if name : ""
+{
+     print("Hello! " + name)
+}
+
+help = input("How can I help you?\n:")
+if help : "How are you?"
+{
+     print("I am fine"),
+     playsound('E://FIne.mp4')
+}
